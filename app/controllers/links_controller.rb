60d8006a975ab0c87e76eddda417019f06a2ea01
ohletamyleta <ohletamyleta@gmail.com>
2021-06-07 13:58:09 -0400

    refactoring

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,8 +1,11 @@
 class LinksController < ApplicationController
 
-  def create 
+  def create
+    original_url = params[:link][:original_url]
+    shortener = Shortener.new(original_url)
+    @link = shortener.generate_short_link
 
   end
-  
+
 end
  
--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -8,10 +8,21 @@ class Shortener
     @url = url
   end 
 
+  def generate_short_link
+    link_model.create(original_url: url, lookup_code: lookup_code)
+
   def lookup_code
-    Digest::SHA256.hexdigest(url)[0..6]
+    loop do
+      code = get_fresh_code
+      break code unless link_model.exists?(lookup_code: code)
+    end
   end
 
+  private
+
+  def get_fresh_code
+    SecureRandom.uuid[0..6]
+  end
 
 end
 
